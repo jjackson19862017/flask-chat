bf551cab3ff9df4cb1c9281bcb3d93c399a4b5ac
Stephen Jackson <jjackson19862017@gmail.com>
2020-01-15 14:23:12 +0000

    Define Message List

--- run.py
+++ run.py
@@ -2,6 +2,11 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+messages = []
+
+def add_messages(username, message):
+    messages.append("{}: {}".format(username, message))
+    print(messages)
 
 @app.route('/')
 def index():
@@ -10,10 +15,12 @@ def index():
 
 @app.route('/<username>')
 def user(username):
-    return "Hi " + username
+    """Display chat messages"""
+    return "Welcome, {0} ".format(username, messages)
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
+    """Create a new message and redirect back to the chat page"""
     return "{0}: {1}".format(username, message)
 
 

1f4c303ae13ce212343075f943052a6e3baa5853
Stephen Jackson <jjackson19862017@gmail.com>
2020-01-15 14:41:41 +0000

    Adding Time Stamp and Creating Index.html

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 messages = []
 
 def add_messages(username, message):
     """Add messages to the 'messages' list"""
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
     print(messages)
 
 def get_all_messages():
@@ -16,7 +18,7 @@ def get_all_messages():
 @app.route('/')
 def index():
     """Main page with instructions """
-    return "To send a message use /USERNAME/MESSAGE"
+    return render_template("index.html")
 
 @app.route('/<username>')
 def user(username):
